6da62e58e1977b1e3ed8c806f0b1658e54fa3a05
vivek-malarventhan <vivek.malarventhan@outlook.com>
2025-12-28 16:14:01 +0530

    Initial Draft

--- lf-design-04-vue/src/api/axios.js
+++ lf-design-04-vue/src/api/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-export const api = axios.create({
+/*export const api = axios.create({
   baseURL: "http://localhost:5000/api",
   withCredentials: true
+});*/
+
+export const api = axios.create({
+    baseURL: "https://catbel.onrender.com/api"
 });
